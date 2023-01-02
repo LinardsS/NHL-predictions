2d169af74be08fdbf70d266a17b8b6cb1bf402bf
LinardsS <linards.solovjevs@gmail.com>
2023-01-02 11:30:04 +0200

    Daily logs added(print reverted to text file instead of console)

--- src/daily.py
+++ src/daily.py
@@ -2,6 +2,13 @@ from downloadCsv import downloadTeamStats
 from uploadResults import uploadResultsAndStats
 from utils import getTodaysDate, getTomorrowsDate
 from predictions import predictSlate, scorePredictions
+import sys
+from os import path
+basepath = path.dirname(__file__)
+todays_date = getTodaysDate()
+todays_date = todays_date + '.txt'
+filepath = path.abspath(path.join(basepath, "..", "data/daily logs", todays_date))
+sys.stdout = open(filepath, 'w')
 
 downloadTeamStats(today = True, date = None, file_date = None) # downloads team stats file
 uploadResultsAndStats() # updates games file with results of last night's games and adds stats for both teams at the time of game
@@ -14,3 +21,5 @@ scorePredictions()
 #predict next night's games
 tomorrows_date = getTomorrowsDate(format = "%Y-%m-%d")
 predictSlate(date = tomorrows_date)
+
+sys.stdout.close()
